e04f6e3f4809f9b0e1c9163ac0e0746df421a071
Meghana <yedlameghana01@gmail.com>
2025-07-11 21:21:05 +0530

    Create 1683. Invalid Tweets.sql

---
+++ 1683. Invalid Tweets.sql
@@ -0,0 +1,18 @@
+1683. Invalid Tweets
+
+Table: Tweets
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+
+
+CODE:
+
+  SELECT tweet_id FROM tweets 
+  WHERE length(content) > 15;
